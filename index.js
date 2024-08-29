609b22db16c55cee50ce02a9eef282a4bcc599a2
msebton-captech <47540179+msebton-captech@users.noreply.github.com>
2024-08-29 14:22:53 +0000

    initial files

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 import * as core from '@actions/core';
+import * as core from '@actions/github';
 
 const name = core.getInput('name');
 const output_value = `Hello ${name}`
 core.setOutput(`Greeting`, output_value);
+
+try {
+    // `who-to-greet` input defined in action metadata file
+    const nameToGreet = core.getInput('who-to-greet');
+    console.log(`Hello ${nameToGreet}!`);
+    const time = (new Date()).toTimeString();
+    core.setOutput("time", time);
+    // Get the JSON webhook payload for the event that triggered the workflow
+    const payload = JSON.stringify(github.context.payload, undefined, 2)
+    console.log(`The event payload: ${payload}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  } 
